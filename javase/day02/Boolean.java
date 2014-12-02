3a5e8f4d5d7d28aa31c6d167d58aced19320733f
yili270 <yili270@163.com>
2014-12-02 17:42:35 +0800

    作业与习题
    作业与习题

---
+++ javase/day02/Boolean.java
@@ -0,0 +1,13 @@
+import java.util.*; 
+public class Boolean{
+	public static void main(String args[]){
+		Scanner in = new Scanner(System.in);
+		System.out.print("print your score:");
+		int score = in.nextInt();
+		int x=-100;
+		String type=score<60?"不及格":"及格";
+		int flag = x>0?1:(x==0?0:-1);
+		System.out.println("type="+type);
+		System.out.println("flag="+flag);
+		}
+}
